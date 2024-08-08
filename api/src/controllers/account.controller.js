511b09820f1b8d2f3766e91304fe151b375cd9b8
24LP9083 24LP9083 <24LP9083@G301-CC0189.local>
2024-08-08 20:05:30 +0800

    suul

--- api/src/controllers/account.controller.js
+++ api/src/controllers/account.controller.js
@@ -31,4 +31,58 @@ const createAccount = async (req, res) => {
   }
 };
 
-module.exports = { getAllAccounts, createAccount };
+const deleteAccount = async (req, res) => {
+  try {
+    const filePath = path.join(__dirname, "..", "data", "account.json");
+    const { id } = req.params;
+
+    const rawData = fs.readFileSync(filePath);
+    let accounts = JSON.parse(rawData);
+
+    accounts = accounts.filter((account) => account.id !== id);
+
+    fs.writeFileSync(filePath, JSON.stringify(accounts, null, 2));
+    res.status(204).end();
+  } catch (error) {
+    console.error("Error deleting account:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+// const fs = require("fs").promises;
+// const path = require("path");
+
+// const deleteAccount = async (req, res) => {
+//   const { id } = req.params;
+
+//   if (!id) {
+//     return res.status(400).json({ error: "Account ID is required" });
+//   }
+
+//   const filePath = path.join(__dirname, "..", "data", "accounts.json");
+
+//   try {
+//     // Файлыг асинхрон унших
+//     const rawData = await fs.readFile(filePath, "utf8");
+//     let accounts = JSON.parse(rawData);
+
+//     // Шинээр данснаас id-ийг устгах
+//     const initialLength = accounts.length;
+//     accounts = accounts.filter((account) => account.id !== id);
+
+//     // Устгасан данс байвал хариулт илгээнэ
+//     if (accounts.length === initialLength) {
+//       return res.status(404).json({ error: "Account not found" });
+//     }
+
+//     // Шинэ өгөгдлийг файлын системд бичих
+//     await fs.writeFile(filePath, JSON.stringify(accounts, null, 2), "utf8");
+
+//     // Амжилттай хариу илгээх
+//     res.status(204).end();
+//   } catch (error) {
+//     console.error("Error deleting account:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
+
+module.exports = { getAllAccounts, createAccount, deleteAccount };
--- api/src/routes/account.route.js
+++ api/src/routes/account.route.js
@@ -2,10 +2,14 @@ const { Router } = require("express");
 const {
   getAllAccounts,
   createAccount,
+  deleteAccount,
 } = require("../controllers/account.controller");
 
 const accountRouter = Router();
 
-accountRouter.get("/", getAllAccounts).post("/", createAccount);
+accountRouter
+  .get("/", getAllAccounts)
+  .post("/", createAccount)
+  .delete("/:id", deleteAccount);
 
 module.exports = { accountRouter };
